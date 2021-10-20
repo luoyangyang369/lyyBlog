d027a02a47b1b86136dc0b807626a442b281ca25
罗洋洋 <luoyangyang163@163.com>
2021-10-20 18:51:13 +0800

    更新博客

--- upgrade.sh
+++ upgrade.sh
@@ -8,6 +8,5 @@ git commit -m "更新博客"
 git push
 
 # 部署流程
-yarn docs:build
 docker build -f docker/Dockerfile -t registry.cn-shenzhen.aliyuncs.com/luoyangyang/luoyangyang:v1 .
 docker push registry.cn-shenzhen.aliyuncs.com/luoyangyang/luoyangyang:v1
